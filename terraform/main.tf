015e8eb9bb678603a818dcd4eac69c157d72be6e
Viliam Gago <viliamgago@Viliams-MacBook-Pro.local>
2025-09-26 12:57:54 +0200

    dbg

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,7 +103,7 @@ resource "azurerm_container_app_environment" "c_app_env" {
   location                   = data.azurerm_resource_group.rg.location
   resource_group_name        = data.azurerm_resource_group.rg.name
   infrastructure_subnet_id   = azurerm_subnet.container_app_subnet.id
-  internal_load_balancer_enabled = false  # Set to false for external access
+  internal_load_balancer_enabled = true  # Set to false for external access
   
   workload_profile {
     name                 = "testwprofile"
